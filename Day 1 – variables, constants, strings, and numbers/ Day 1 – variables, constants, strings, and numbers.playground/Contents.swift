8e460c88aa62156143621aea9c1281322fe1d86b
Tolga Pirtürk <tpirturk@gmail.com>
2021-11-12 02:32:57 +0300

    Doubles
    Add examples of Doubles.

--- Day 1 – variables, constants, strings, and numbers/ Day 1 – variables, constants, strings, and numbers.playground/Contents.swift
+++ Day 1 – variables, constants, strings, and numbers/ Day 1 – variables, constants, strings, and numbers.playground/Contents.swift
@@ -62,3 +62,19 @@ counter /= 2
 let number = 120
 print(number.isMultiple(of: 3))
 print(120.isMultiple(of: 3))
+
+let number2 = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+let c = a + Int(b)
+let d = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var rating = 5.0
+rating *= 2
