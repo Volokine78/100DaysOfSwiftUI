4339c3d38ad7eb82fc73ee7c34cfd6a97627247d
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-02 22:35:36 +0300

    Booleans
    Add examples of boolean type.

--- Day 2 – Booleans, string interpolation, and checkpoint 1/Day 2 – Booleans, string interpolation, and checkpoint 1.playground/Contents.swift
+++ Day 2 – Booleans, string interpolation, and checkpoint 1/Day 2 – Booleans, string interpolation, and checkpoint 1.playground/Contents.swift
@@ -1,3 +1,24 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+let goodDogs = true
+let theGameOver = false
+
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+var gameOver = false
+print(gameOver)
+
+gameOver.toggle()
+print(gameOver)
