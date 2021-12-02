f3a71060964b9e9422f8a2c7a43d52d031d56669
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-03 02:06:19 +0300

    Arrays
    Add examples of arrays and array functionalities.

---
+++ Day 3 – Arrays, dictionaries, sets, and enums/Day 3 – Arrays, dictionaries, sets, and enums.playground/Contents.swift
@@ -0,0 +1,53 @@
+import Cocoa
+
+var beatles = ["John", "Paul", "George", "Ringo"]
+let numbers = [4, 8, 15, 16, 23, 42]
+var temperatures = [25.3, 28.2, 26.4]
+
+print(beatles[0])
+print(numbers[1])
+print(temperatures[2])
+
+beatles.append("Adrian")
+beatles.append("Allen")
+beatles.append("Novall")
+beatles.append("Vivian")
+
+var scores = Array<Int>()
+scores.append(100)
+scores.append(80)
+scores.append(85)
+print(scores[1])
+
+var albums = Array<String>()
+albums.append("Folklore")
+albums.append("Fearless")
+albums.append("Red")
+
+var albums1 = [String]()
+var albums2 = ["Folklore"]
+albums2.append("Fearless")
+albums2.append("Red")
+print(albums2.count)
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+
+characters.remove(at: 2)
+print(characters.count)
+
+characters.removeAll()
+print(characters.count)
+
+let bondMovies = ["Casino Royale", "Spectre", "No Time To Die"]
+print(bondMovies.contains("Frozen"))
+
+let cities = ["London", "Tokyo", "Rome", "Budapest"]
+print(cities.sorted())
+
+let presidents = ["Bush", "Obama", "Trump", "Biden"]
+let reversedPresidents = presidents.reversed()
+print(reversedPresidents)
+print(reversedPresidents.endIndex)
+print(reversedPresidents.first!)
+print(reversedPresidents.startIndex)
