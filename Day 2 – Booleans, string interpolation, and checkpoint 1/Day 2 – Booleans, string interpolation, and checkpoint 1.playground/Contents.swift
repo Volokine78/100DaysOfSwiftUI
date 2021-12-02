54170d16424a1b37b36c4b7451911e4db38fe60d
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-02 23:00:35 +0300

    Join strings and string interpolation
    Add examples of combining strings and string interpolation.

--- Day 2 – Booleans, string interpolation, and checkpoint 1/Day 2 – Booleans, string interpolation, and checkpoint 1.playground/Contents.swift
+++ Day 2 – Booleans, string interpolation, and checkpoint 1/Day 2 – Booleans, string interpolation, and checkpoint 1.playground/Contents.swift
@@ -22,3 +22,26 @@ print(gameOver)
 
 gameOver.toggle()
 print(gameOver)
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+let quote = "Then he tapped a sign saying \"Believe\" and walked away"
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+let no = 11
+let missionMessage = "Apollo " + String(no) + " landed on the moon"
+let missionMessage1 = "Apollo \(no) landed on the moon"
+print(missionMessage1)
+print("5 x 5 is \(5 * 5)")
