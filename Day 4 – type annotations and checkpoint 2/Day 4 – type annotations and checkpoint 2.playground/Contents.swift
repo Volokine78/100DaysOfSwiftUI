e57348bbdb29f82272dade6165c6bc23a5daa750
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-04 02:05:30 +0300

    Type Annotations
    Create variables with type annotations.

--- Day 4 – type annotations and checkpoint 2/Day 4 – type annotations and checkpoint 2.playground/Contents.swift
+++ Day 4 – type annotations and checkpoint 2/Day 4 – type annotations and checkpoint 2.playground/Contents.swift
@@ -1,3 +1,26 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let surname: String = "Lasso"
+var score: Int = 0
+
+var score1: Double = 0
+let playerName: String = "Roy"
+var luckNumber: Int = 13
+let pi: Double = 3.141
+var isAuthenticated: Bool = true
+
+var albums: [String] = ["Red", "Fearless"]
+var user: [String: String] = ["id": "@twostraws"]
+var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"])
+var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
+
+var teams: [String] = [String]()
+var cities: [String] = []
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style = UIStyle.light
+style = .dark
