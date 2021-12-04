a8ad782a89a790367b3f6dbc2d0d7caa6f6d5aea
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-04 03:52:03 +0300

    Multiple conditions
    Add if else statements for multiple conditions.

--- Day 5 – if, switch, and the ternary operator/Day 5 – if, switch, and the ternary operator.playground/Contents.swift
+++ Day 5 – if, switch, and the ternary operator/Day 5 – if, switch, and the ternary operator.playground/Contents.swift
@@ -64,3 +64,40 @@ if username.isEmpty {
     username = "Anonymous"
     print("Enter your user name")
 }
+
+let age1 = 16
+
+if age1 >= 18 {
+    print("You can vote in the next election.")
+} else {
+    print("Sorry, you're too young to vote.")
+}
+
+let temp = 25
+
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+if userAge >= 18 || hasParentalConsent {
+    print("You can buy the game")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path.")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
