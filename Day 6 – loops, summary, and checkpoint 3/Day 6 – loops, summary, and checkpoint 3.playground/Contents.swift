50ad954d473949d116c5ab2330be5ef7924c2c9a
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-05 01:53:46 +0300

    For Loop
    Add examples using for loop.

--- Day 6 – loops, summary, and checkpoint 3/Day 6 – loops, summary, and checkpoint 3.playground/Contents.swift
+++ Day 6 – loops, summary, and checkpoint 3/Day 6 – loops, summary, and checkpoint 3.playground/Contents.swift
@@ -1,3 +1,50 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for os in platforms {
+    print("Swift works great on \(os)")
+}
+
+for i in 1...12 {
+    print("5 x \(i) is \(5 * i)")
+}
+
+print()
+
+for i in 1...12 {
+    print("The \(i) times table:")
+
+    for j in 1...12 {
+        print("  \(j) x \(i) is \(j * i)")
+    }
+
+    print()
+}
+
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
+
+print()
+
+for i in 1..<5 {
+    print("Counting 1 up to 5: \(i)")
+}
+
+print()
+
+var lyric = "Haters gonna"
+
+for _ in 1...5 {
+    lyric += " hate"
+}
+
+print(lyric)
+
+let names = ["Piper", "Alex", "Suzanne", "Gloria"]
+
+print(names[0])
+print(names[1...3])
+print(names[0...])
+
