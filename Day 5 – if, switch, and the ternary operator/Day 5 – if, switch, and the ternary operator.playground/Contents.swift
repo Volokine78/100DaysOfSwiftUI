9a53da08ef2095cf0c600661f424c4290d4d69d1
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-05 01:14:09 +0300

    Ternary Operator
    Add examples of ternary conditional operator.

--- Day 5 – if, switch, and the ternary operator/Day 5 – if, switch, and the ternary operator.playground/Contents.swift
+++ Day 5 – if, switch, and the ternary operator/Day 5 – if, switch, and the ternary operator.playground/Contents.swift
@@ -153,3 +153,25 @@ switch day {
     default:
         print("A partridge in a pear tree")
 }
+
+let age2 = 18
+let canVote = age2 >= 18 ? "Yes" : "No"
+
+let hour = 23
+print(hour < 12 ? "It's before noon" : "It's after noon")
+
+let names = ["Jayne", "Kaylee", "Mal"]
+let crewCount = names.isEmpty ? "No one" : "\(names.count) people"
+print(crewCount)
+
+enum Theme {
+    case light, dark
+}
+
+let theme = Theme.dark
+
+let background = theme == .dark ? "black" : "white"
+print(background)
+
+let isAuthenticated = true
+print(isAuthenticated ? "Welcome!" : "Who are you?")
