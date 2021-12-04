ebf93589cdc6af5bff5b021f8e2df5d3c91b6492
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-05 02:27:03 +0300

    While Loop
    Add examples using while loop.

--- Day 6 – loops, summary, and checkpoint 3/Day 6 – loops, summary, and checkpoint 3.playground/Contents.swift
+++ Day 6 – loops, summary, and checkpoint 3/Day 6 – loops, summary, and checkpoint 3.playground/Contents.swift
@@ -48,3 +48,26 @@ print(names[0])
 print(names[1...3])
 print(names[0...])
 
+print()
+
+var countdown = 10
+
+while countdown > 0 {
+    print("\(countdown)...")
+    countdown -= 1
+}
+
+print("Blast off!")
+
+let id = Int.random(in: 1...1000)
+
+let amount = Double.random(in: 0...1)
+
+var roll = 0
+
+while roll != 20 {
+    roll = Int.random(in: 1...20)
+    print("I rolled a \(roll)")
+}
+
+print("Critical hit!")
