9157f4a88b56a98e8b2960c8aee31b4b42b591de
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-05 03:21:52 +0300

    Checkpoint 3
    Add solution of FizzBuzz problem.

--- Day 6 – loops, summary, and checkpoint 3/Day 6 – loops, summary, and checkpoint 3.playground/Contents.swift
+++ Day 6 – loops, summary, and checkpoint 3/Day 6 – loops, summary, and checkpoint 3.playground/Contents.swift
@@ -97,3 +97,44 @@ for i in 1...100_000 {
 }
 
 print(multiples)
+print()
+
+// Checkpoint 3
+for i in 1...100 {
+    if i % 3 == 0 && i % 5 == 0 {
+        print("FizzBuzz")
+    } else if i % 3 == 0 {
+        print("Fizz")
+    } else if i % 5 == 0 {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
+print()
+
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if i.isMultiple(of: 3) {
+        print("Fizz")
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
+print()
+
+for i in 1...100 {
+    switch i {
+        case _ where i.isMultiple(of: 3) && i.isMultiple(of: 5):
+            print("FizzBuzz")
+        case _ where i.isMultiple(of: 5):
+            print("Buzz")
+        case _ where i.isMultiple(of: 3):
+            print("Fizz")
+        default:
+            print(i)
+    }
+}
