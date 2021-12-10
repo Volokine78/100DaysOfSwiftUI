e19613f225bcd4292d572849ee0ebd82775a3249
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-11 02:02:19 +0300

    Customize parameter labels
    Add function examples with custom parameter labels.

--- Day 7 – functions, parameters, and return values/Day 7 – functions, parameters, and return values.playground/Contents.swift
+++ Day 7 – functions, parameters, and return values/Day 7 – functions, parameters, and return values.playground/Contents.swift
@@ -96,3 +96,37 @@ print("Name: \(user5.0) \(user5.1)")
 
 let (firstName2, _) = getUser3()
 print("Name: \(firstName2)")
+
+func rollDice(sides: Int, count: Int) -> [Int] {
+    var rolls = [Int]()
+    
+    for _ in 1...count {
+        let roll = Int.random(in: 1...sides)
+        rolls.append(roll)
+    }
+    return rolls
+}
+
+let rolls = rollDice(sides: 6, count: 4)
+
+func hireEmployee(name: String) { }
+func hireEmployee(title: String) { }
+func hireEmployee(location: String) { }
+
+let lyric = "I see a red door and I want it painted black"
+print(lyric.hasPrefix("I see"))
+
+func isUppercase2(_ string: String) -> Bool {
+    string == string.uppercased()
+}
+
+let string = "HELLO, WORLD"
+let result2 = isUppercase2(string)
+
+func printTimesTables(for number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5)
