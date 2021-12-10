e6193e3d1fde66e8598ef88a26273172ce888e58
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-10 23:34:00 +0300

    Functions
    Add examples of functions.

--- Day 7 – functions, parameters, and return values/Day 7 – functions, parameters, and return values.playground/Contents.swift
+++ Day 7 – functions, parameters, and return values/Day 7 – functions, parameters, and return values.playground/Contents.swift
@@ -1,3 +1,19 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+func showWelcome() {
+    print("Welcome to my app!")
+    print("By default This prints out a conversion")
+    print("chart from centimeters to inches, but you")
+    print("can also set a custom range if you want.")
+}
+showWelcome()
+
+let roll = Int.random(in: 1...20)
+
+func printTimesTables(number: Int, end: Int) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(number: 5, end: 20)
