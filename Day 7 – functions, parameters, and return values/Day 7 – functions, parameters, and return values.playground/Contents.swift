3b5316c4f089988906cf0717b7e54782f62ba624
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-11 01:11:05 +0300

    Return multiple values from functions
    Add examples of functions, returns multiple values.

--- Day 7 – functions, parameters, and return values/Day 7 – functions, parameters, and return values.playground/Contents.swift
+++ Day 7 – functions, parameters, and return values/Day 7 – functions, parameters, and return values.playground/Contents.swift
@@ -48,3 +48,51 @@ func helloWorld() {
 }
 
 helloWorld()
+
+func isUppercase(string: String) -> Bool {
+    string == string.uppercased()
+}
+
+func getUser() -> [String] {
+    ["Taylor", "Swift"]
+}
+
+let user = getUser()
+print("Name: \(user[0]) \(user[1])")
+
+func getUser2() -> [String: String] {
+    [
+        "firstName": "Taylor",
+        "lastName": "Swift"
+    ]
+}
+
+let user2 = getUser2()
+print("Name: \(user2["firstName", default: "Anonymous"]) \(user2["lastName", default: "Anonymous"])")
+
+func getUser3() -> (firstName: String, lastName: String) {
+    (firstName: "Taylor", lastName: "Swift")
+}
+
+let user3 = getUser3()
+let (firstName, lastName) = getUser3()
+print("Name: \(firstName) \(lastName)")
+
+let firstName1 = user3.firstName
+let lastName1 = user3.lastName
+print("Name: \(firstName1) \(lastName1)")
+print("Name: \(user3.firstName) \(user3.lastName)")
+
+func getUser4() -> (firstName: String, lastName: String) {
+    ("Taylor", "Swift")
+}
+
+func getUser5() -> (String, String) {
+    ("Taylor", "Swift")
+}
+
+let user5 = getUser5()
+print("Name: \(user5.0) \(user5.1)")
+
+let (firstName2, _) = getUser3()
+print("Name: \(firstName2)")
