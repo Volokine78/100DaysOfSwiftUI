0ee43dfd141a7b0da73e7e149c8afa06eab91780
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-11 03:17:19 +0300

    Handle errors in functions
    Add functions with error handling.

--- Day 8 – default values, throwing functions, and checkpoint 4/Day 8 – default values, throwing functions, and checkpoint 4.playground/Contents.swift
+++ Day 8 – default values, throwing functions, and checkpoint 4/Day 8 – default values, throwing functions, and checkpoint 4.playground/Contents.swift
@@ -13,3 +13,50 @@ var characters = ["Lana", "Pam", "Ray", "Sterling"]
 print(characters.count)
 characters.removeAll(keepingCapacity: true)
 print(characters.count)
+
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+    
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+    
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+
+let string = "12345"
+let password = "fs445s6f4s55"
+let password2 = "25"
+
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error.")
+}
+
+do {
+    let result = try checkPassword(password2)
+    print("Password rating: \(result)")
+} catch {
+    print("There was an error \(error.localizedDescription)")
+}
+
+let result2 = try! checkPassword(password)
+print(result2)
