f9c0d48f78bd0bb52ccb4e4c9344f7237b230ad2
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-13 01:47:50 +0300

    Closures
    Create and use closures.

--- Day 9 – closures, passing functions into functions, and checkpoint 5/Day 9 – closures, passing functions into functions, and checkpoint 5.playground/Contents.swift
+++ Day 9 – closures, passing functions into functions, and checkpoint 5/Day 9 – closures, passing functions into functions, and checkpoint 5.playground/Contents.swift
@@ -1,3 +1,64 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+func greetUser() {
+    print("Hi there!")
+}
+
+greetUser()
+
+var greetCopy = greetUser
+greetCopy()
+var greetCopy2: () -> Void = greetUser
+
+let sayHello = {
+    print("Hi there!")
+}
+
+sayHello()
+
+let sayHelloClosure = { (name: String) -> String in
+    "Hi \(name)!"
+}
+
+func getUserData(for id: Int) -> String {
+    if id == 1989 {
+        return "Taylor Swift"
+    } else {
+        return "Anonymous"
+    }
+}
+
+let data: (Int) -> String = getUserData
+let user = data(1989)
+print(user)
+
+print(sayHelloClosure("Taylor"))
+
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+let sortedTeam = team.sorted()
+print(sortedTeam)
+
+func captianFirstSorted(name1: String, name2: String) -> Bool {
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    
+    return name1 < name2
+}
+
+let captainFirstTeam = team.sorted(by: captianFirstSorted)
+print(captainFirstTeam)
+
+let captainFirstTeamClosure = team.sorted(by: { (name1: String, name2: String) -> Bool in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+    
+    return name1 < name2
+})
+
+print(captainFirstTeamClosure)
