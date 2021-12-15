0a9ad77b7150b301fc7de9418b67760c55bda885
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-16 02:23:12 +0300

    Static properties and methods
    Add custom structs with static properties and methods.

--- Day 11 – access control, static properties and methods, and checkpoint 6/Day 11 – access control, static properties and methods, and checkpoint 6.playground/Contents.swift
+++ Day 11 – access control, static properties and methods, and checkpoint 6/Day 11 – access control, static properties and methods, and checkpoint 6.playground/Contents.swift
@@ -28,3 +28,35 @@ if success {
 }
 
 print(account.funds)
+
+struct School {
+    static var studentCount = 0
+    var teacherCount = studentCount + 5
+    
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1
+    }
+}
+
+School.add(student: "Taylor Swift")
+print(School.studentCount)
+let totalCount = School()
+totalCount.teacherCount
+
+struct AppData {
+    static let version = "1.3 beta 2"
+    static let saveFilename = "settings.json"
+    static let homeURL = "https://www.hackingwithswift.com"
+}
+
+AppData.version
+
+struct Employee {
+    let username: String
+    let password: String
+    
+    static let example = Employee(username: "cfederighi", password: "h4irf0rce0ne")
+}
+
+Employee.example
