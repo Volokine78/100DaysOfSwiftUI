06d3d815fa85e7b88a9f34e1707713bd29e690b9
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-16 01:44:46 +0300

    Access control
    Add a struct example by using access control.

--- Day 11 – access control, static properties and methods, and checkpoint 6/Day 11 – access control, static properties and methods, and checkpoint 6.playground/Contents.swift
+++ Day 11 – access control, static properties and methods, and checkpoint 6/Day 11 – access control, static properties and methods, and checkpoint 6.playground/Contents.swift
@@ -1,3 +1,30 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+struct BankAccount {
+    private(set) var funds = 0
+    
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds > amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount: 100)
+let success = account.withdraw(amount: 200)
+
+if success {
+    print("Withdraw money successfully")
+} else {
+    print("Failed to get the money")
+}
+
+print(account.funds)
