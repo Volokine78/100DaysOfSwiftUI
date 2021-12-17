b2ccc9f7c05b280fe2344d7ceba431937c1da49d
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-18 02:24:53 +0300

    Classes
    Add an example of custom class.

--- Day 12 – classes, inheritance, and checkpoint 7/Day 12 – classes, inheritance, and checkpoint 7.playground/Contents.swift
+++ Day 12 – classes, inheritance, and checkpoint 7/Day 12 – classes, inheritance, and checkpoint 7.playground/Contents.swift
@@ -1,3 +1,12 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+class Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var newGame = Game()
+newGame.score += 10
