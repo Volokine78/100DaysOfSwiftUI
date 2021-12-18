b80d0b4f4d26e0f3a878cb57000b580722658511
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-18 05:07:36 +0300

    Checkpoint 7
    Add solution for checkpoint 7

--- Day 12 – classes, inheritance, and checkpoint 7/Day 12 – classes, inheritance, and checkpoint 7.playground/Contents.swift
+++ Day 12 – classes, inheritance, and checkpoint 7/Day 12 – classes, inheritance, and checkpoint 7.playground/Contents.swift
@@ -119,3 +119,69 @@ var user4 = User2()
 user4.name = "Taylor"
 user4 = User2()
 print(user4.name)
+
+// MARK: - Checkpoint 7
+class Animal {
+    let legs: Int
+    
+    init(legs: Int) {
+        self.legs = legs
+    }
+}
+
+class Dog: Animal {
+    func speak() {
+        print("My dog greets you with row")
+    }
+}
+
+class Corgi: Dog {
+    override func speak() {
+        print("bark bark row")
+    }
+}
+
+class Poodle: Dog {
+    override func speak() {
+        print("bork bork raw")
+    }
+}
+
+class Cat: Animal {
+    let isTame: Bool
+    
+    func speak() {
+        print("Give me all your attention and obey me meow")
+    }
+    
+    init(isTame: Bool, legs: Int) {
+        self.isTame = isTame
+        super.init(legs: legs)
+    }
+}
+
+class Persian: Cat {
+    override func speak() {
+        print("What a wonderful day prrrr")
+    }
+}
+
+class Lion: Cat {
+    override func speak() {
+        print("I can eat you easly but you're not worth it roarr")
+    }
+}
+
+let dog = Dog(legs: 4)
+dog.speak()
+let corgi = Corgi(legs: 4)
+corgi.speak()
+let poodle = Poodle(legs: 4)
+poodle.speak()
+
+let cat = Cat(isTame: true, legs: 4)
+cat.speak()
+let persian = Persian(isTame: true, legs: 4)
+persian.speak()
+let king = Lion(isTame: false, legs: 4)
+king.speak()
