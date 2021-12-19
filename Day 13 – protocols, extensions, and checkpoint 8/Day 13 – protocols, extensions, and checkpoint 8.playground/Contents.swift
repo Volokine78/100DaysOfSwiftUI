2cc072c81104f067f652866d6be996ee5b585055
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-20 02:25:00 +0300

    Extensions
    Add examples of extensions to data types and custom types.

--- Day 13 – protocols, extensions, and checkpoint 8/Day 13 – protocols, extensions, and checkpoint 8.playground/Contents.swift
+++ Day 13 – protocols, extensions, and checkpoint 8/Day 13 – protocols, extensions, and checkpoint 8.playground/Contents.swift
@@ -69,3 +69,55 @@ func getRandomBool() -> some Equatable {
 }
 
 print(getRandomNumber() == getRandomNumber())
+
+var quote = "   The truth is rarely pure and never simple   "
+let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
+
+extension String {
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    mutating func trim() {
+        self = self.trimmed()
+    }
+    
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
+    }
+}
+
+func trim(_ string: String) -> String {
+    string.trimmingCharacters(in: .whitespacesAndNewlines)
+}
+
+let trimmed2 = quote.trimmed()
+let trimmed3 = trim(quote)
+quote.trim()
+
+let lyrcis = """
+But I keep cruising
+Can't stop, won't stop moving
+It's like I got this music in my mind
+Saying it's gonna be alright
+"""
+
+print(lyrcis.lines.count)
+
+struct Book {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+}
+
+extension Book {
+    init(title: String, pageCount: Int) {
+        self.title = title
+        self.pageCount = pageCount
+        self.readingHours = pageCount / 50
+    }
+}
+
+let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
+let dune = Book(title: "Dune", pageCount: 700)
+dune.readingHours
