cb3591e27b2fbf17f13e8f635e23e1b750c70931
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-20 02:53:45 +0300

    Protocol extensions
    Add an example of protocol extension.

--- Day 13 – protocols, extensions, and checkpoint 8/Day 13 – protocols, extensions, and checkpoint 8.playground/Contents.swift
+++ Day 13 – protocols, extensions, and checkpoint 8/Day 13 – protocols, extensions, and checkpoint 8.playground/Contents.swift
@@ -121,3 +121,33 @@ extension Book {
 let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
 let dune = Book(title: "Dune", pageCount: 700)
 dune.readingHours
+
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+
+let guests = ["Mario", "Luigi", "Peach"]
+
+if guests.isNotEmpty {
+    print("Guest count: \(guests.count)")
+}
+
+protocol Person {
+    var name: String { get }
+    func sayHello()
+}
+
+extension Person {
+    func sayHello() {
+        print("Hi, I'm \(name)")
+    }
+}
+
+struct Employee: Person {
+    let name: String
+}
+
+let taylor = Employee(name: "Taylor Swift")
+taylor.sayHello()
