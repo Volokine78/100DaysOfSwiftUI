ba1fce30b63d4c2d21028979b5ca72d08efe60d7
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-20 05:03:57 +0300

    Nil coalescing
    Add examples of unwrapping optionals with nil coalescing.

--- Day 14 – optionals, nil coalescing, and checkpoint 9/Day 14 – optionals, nil coalescing, and checkpoint 9.playground/Contents.swift
+++ Day 14 – optionals, nil coalescing, and checkpoint 9/Day 14 – optionals, nil coalescing, and checkpoint 9.playground/Contents.swift
@@ -33,3 +33,27 @@ func printSquare(of number: Int?) {
     
     print("\(number) x \(number) is \(number * number)")
 }
+
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
+
+let new = captains["Serenity"] ?? "N/A"
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+let input = ""
+let number1 = Int(input) ?? 0
+print(number1)
