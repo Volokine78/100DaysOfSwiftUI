f9516f2bc9b606c7cc1e5ad2f4c6fc1a0dd01ef3
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-20 04:23:54 +0300

    Optionals
    Add examples of optionals.

--- Day 14 – optionals, nil coalescing, and checkpoint 9/Day 14 – optionals, nil coalescing, and checkpoint 9.playground/Contents.swift
+++ Day 14 – optionals, nil coalescing, and checkpoint 9/Day 14 – optionals, nil coalescing, and checkpoint 9.playground/Contents.swift
@@ -1,3 +1,27 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let opposites = ["Mario": "Wario", "Luigi": "Waluigi"]
+let peachOpposite = opposites["Peach"]
+
+if let marioOpposite = opposites["Mario"] {
+    print("Mario's opposite is \(marioOpposite)")
+}
+
+var username: String? = nil
+
+if let unwrappedName = username {
+    print("We got a user: \(unwrappedName)")
+} else {
+    print("The optional was empty.")
+}
+
+func square(number: Int) -> Int {
+    number * number
+}
+
+var number: Int? = nil
+number = 5
+if let number = number {
+    print(square(number: number))
+}
+
