e3a28fcb2f16ae54d4605769f80ab0d0161c622f
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-21 01:56:58 +0300

    Day 16
    Add WeSplit project.

---
+++ Day 16 – Project 1, part one/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tolga PIRTURK on 21.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
