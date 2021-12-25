7047c6f01ede1b5a1613c1186d8cc2977b8842d8
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-25 23:00:16 +0300

    Day 19
    Add UnitConverter project file.

---
+++ Day 19 – Challenge day/UnitConverter/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Tolga PIRTURK on 25.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
