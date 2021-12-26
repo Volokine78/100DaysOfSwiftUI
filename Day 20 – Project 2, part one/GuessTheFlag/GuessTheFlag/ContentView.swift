d743f4a77030d8f3d0356bb636d2d15645ae4f50
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-26 03:00:06 +0300

    Using stacks to arrange views
    Create 3x3 grid with vertical and horizontal stacks.

--- Day 20 – Project 2, part one/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Day 20 – Project 2, part one/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,8 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 10) {
+            HStack {
+                Rectangle()
+                    .fill(Color.blue)
+                Rectangle()
+                    .fill(Color.blue)
+                Rectangle()
+                    .fill(Color.blue)
+            }
+            .padding(.leading, 10)
+            .padding(.trailing, 10)
+            
+            HStack {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.green)
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.green)
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.green)
+            }
+            .padding(.leading, 10)
+            .padding(.trailing, 10)
+            
+            HStack {
+                Capsule()
+                    .fill(Color.red)
+                Capsule()
+                    .fill(Color.red)
+                Capsule()
+                    .fill(Color.red)
+            }
+            .padding(.leading, 10)
+            .padding(.trailing, 10)
+        }
     }
 }
 
