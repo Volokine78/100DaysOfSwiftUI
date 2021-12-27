05377d8c1fd14b82ae8325784197d37aa953658c
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-28 00:09:52 +0300

    View composition
    Create custom view to split up one large view into smaller views.

--- Day 23 – Project 3, part one/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Day 23 – Project 3, part one/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,29 +7,25 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var motto1: some View {
-        Text("Draco dormiens")
-    }
-    
-    let motto2 = Text("nunquam titillandus")
-    
-    var spells: some View {
-        Group {
-            Text("Lumos")
-            Text("Obliviate")
-        }
-    }
-    
-    @ViewBuilder var spells2: some View {
-            Text("Lumos")
-            Text("Obliviate")
-    }
+struct CapsuleText: View {
+    var text: String
     
     var body: some View {
-        VStack {
-            spells
-            spells2
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+                .foregroundColor(.white)
+            CapsuleText(text: "Second")
+                .foregroundColor(.yellow)
         }
     }
 }
