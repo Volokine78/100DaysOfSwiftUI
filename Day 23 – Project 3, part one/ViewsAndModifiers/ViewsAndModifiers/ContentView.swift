c5d792b952a110ef712432f8edd708f4bf11283a
Tolga Pirtürk <tpirturk@gmail.com>
2021-12-27 03:33:56 +0300

    Modifier order
    Add examples for how modifier order change SwiftUI view.

--- Day 23 – Project 3, part one/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Day 23 – Project 3, part one/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            Button("Hello, world!") {
+                print(type(of: self.body))
+            }
             .background(.red)
+            .frame(width: 200, height: 200)
+            
+            Button("Hello, world!") {
+                print(type(of: self.body))
+            }
+            .frame(width: 200, height: 200)
+            .background(.red)
+            
+            Text("Hello, world!")
+                .padding()
+                .background(.red)
+                .padding()
+                .background(.blue)
+                .padding()
+                .background(.green)
+                .padding()
+                .background(.yellow)
+        }
     }
 }
 
