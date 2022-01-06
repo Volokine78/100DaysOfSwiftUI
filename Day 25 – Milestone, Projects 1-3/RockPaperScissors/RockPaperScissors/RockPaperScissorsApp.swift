3c36ff310eb163cd2c466b451c59b48bce0523ea
Tolga Pirtürk <tpirturk@gmail.com>
2022-01-07 00:42:20 +0300

    Day 25
    Add RockPaperScissors project file.

---
+++ Day 25 – Milestone, Projects 1-3/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Tolga PIRTURK on 7.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
