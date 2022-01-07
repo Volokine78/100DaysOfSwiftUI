019ab186716058c8d99e55abd27244493800f0f4
Tolga Pirtürk <tpirturk@gmail.com>
2022-01-08 00:22:58 +0300

    Working with dates
    Add examples of how to use Date type and DateComponents.

--- Day 26 – Project 4, part one/BetterRest/BetterRest/ContentView.swift
+++ Day 26 – Project 4, part one/BetterRest/BetterRest/ContentView.swift
@@ -11,7 +11,24 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     
     var body: some View {
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+        VStack {
+            Text(Date.now, format: .dateTime.hour().minute())
+            Text(Date.now, format: .dateTime.day().month().year())
+            Text(Date.now.formatted(date: .long, time: .shortened))
+        }
+    }
+    
+    func trivialExample() {
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        let date = Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    func trivialExample2() {
+        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
