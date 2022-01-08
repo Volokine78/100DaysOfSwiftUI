69d53cc6ca8a51c5f939992debf90290628f9ca4
Tolga Pirtürk <tpirturk@gmail.com>
2022-01-09 01:46:19 +0300

    Working with strings
    Add components(separatedBy:) method to convert single string to an array of strings. Add trimmingCharacters(in:) method to remove certain kind of characters from the start and end of a string. Use UITextChecker object to check for misspelled words.

--- Day 29 – Project 5, part one/WordScramble/WordScramble/ContentView.swift
+++ Day 29 – Project 5, part one/WordScramble/WordScramble/ContentView.swift
@@ -11,15 +11,28 @@ struct ContentView: View {
     let people = ["Finn", "Leia", "Luke", "Rey"]
     
     var body: some View {
-        Text("Hello world")
+        Text("Hello, world!")
+            .padding()
     }
     
-    func loadFile() {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                fileContents
-            }
-        }
+    func test() {
+        let input = """
+a
+b
+c
+"""
+        let letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
     }
 }
 
