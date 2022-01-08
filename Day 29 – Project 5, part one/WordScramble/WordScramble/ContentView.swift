fc7377965b2dbdbd3346deb41df6676826ced91a
Tolga Pirtürk <tpirturk@gmail.com>
2022-01-09 00:42:44 +0300

    List
    Add example for how to use List.

--- Day 29 – Project 5, part one/WordScramble/WordScramble/ContentView.swift
+++ Day 29 – Project 5, part one/WordScramble/WordScramble/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(people, id: \.self) {
+            Text($0)
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+
+            Section("Section 2") {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+
+            Section("Section 3") {
+                Text("Static row 3")
+                Text("Static row 4")
+            }
+        }
+        .listStyle(.grouped)
     }
 }
 
