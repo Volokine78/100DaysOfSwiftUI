393900e445548d3c02059e49baec5da8c67cee0b
Tolga Pirtürk <tpirturk@gmail.com>
2022-01-09 01:50:57 +0300

    Day 30
    Add WordScrambler project file.

---
+++ Day 30 – Project 5, part two/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Tolga PIRTURK on 8.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+    
+    func test() {
+        let input = """
+a
+b
+c
+"""
+        let letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
