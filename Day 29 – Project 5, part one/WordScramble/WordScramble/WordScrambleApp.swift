027b215bfe993936c84e54c0822cb19c9f69ed1f
Tolga Pirtürk <tpirturk@gmail.com>
2022-01-08 23:34:46 +0300

    Day 29
    Add WordScramble project file.

---
+++ Day 29 – Project 5, part one/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Tolga PIRTURK on 8.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
