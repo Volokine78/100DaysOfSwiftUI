0e95dcb6a782bc8b68b7c602632e00fb30e93e5a
Tolga Pirtürk <tpirturk@gmail.com>
2022-01-10 02:34:09 +0300

    Day 33
    Add Animations project file.

---
+++ Day 33 – Project 6, part two/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Tolga PIRTURK on 10.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
