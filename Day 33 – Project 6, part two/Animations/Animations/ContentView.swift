d4a09cb65e8e6ecf43725052eb08d037d06a71f9
Tolga Pirtürk <tpirturk@gmail.com>
2022-01-10 23:42:24 +0300

    Animating gestures
    Add a view with drag gesture and add animation when gesture ends. Add an array of string and for every letter add an animation. Add drag gesture to stack which will cause a snake like effect with animation. Color of letters will also change as user release the drag.

--- Day 33 – Project 6, part two/Animations/Animations/ContentView.swift
+++ Day 33 – Project 6, part two/Animations/Animations/ContentView.swift
@@ -8,18 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var dragAmount = CGSize.zero
     @State private var enabled = false
     
+    let letters = Array("Hello, SwiftUI")
+    
     var body: some View {
-        Button("Tap Me") {
-            enabled.toggle()
+        VStack(spacing: 50) {
+            LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .frame(width: 300, height: 200)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .offset(dragAmount)
+                .gesture(
+                    DragGesture()
+                        .onChanged { dragAmount = $0.translation }
+                        .onEnded { _ in
+                            withAnimation(.spring()) {
+                                dragAmount = .zero }
+                        }
+                )
+            
+            HStack(spacing: 0) {
+                ForEach(0..<letters.count) { num in
+                    Text(String(letters[num]))
+                        .padding(5)
+                        .font(.title)
+                        .background(enabled ? .blue : .red)
+                        .offset(dragAmount)
+                        .animation(
+                            .default.delay(Double(num) / 20),
+                            value: dragAmount
+                        )
+                }
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in
+                        dragAmount = .zero
+                        enabled.toggle()
+                    }
+            )
         }
-        .frame(width: 200, height: 200)
-        .background(enabled ? .blue : .red)
-        .animation(.default, value: enabled)
-        .foregroundColor(.white)
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
     }
 }
 
