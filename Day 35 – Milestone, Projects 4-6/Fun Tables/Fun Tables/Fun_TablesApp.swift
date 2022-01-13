b7f304993af65eeb8eacac659d451155d1f280f7
Tolga Pirtürk <tpirturk@gmail.com>
2022-01-14 02:18:05 +0300

    Day 35
    Add Fun Tables project file for the challenge.

---
+++ Day 35 – Milestone, Projects 4-6/Fun Tables/Fun Tables/Fun_TablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_TablesApp.swift
+//  Fun Tables
+//
+//  Created by Tolga PIRTURK on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Fun_TablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
