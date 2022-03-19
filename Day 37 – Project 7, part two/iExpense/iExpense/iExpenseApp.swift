8d7709e3e1c225a849dcdbe74e558fdfa738e1c3
Tolga Pirtürk <tpirturk@gmail.com>
2022-03-19 18:18:50 +0300

    Day 37
    Add Project file for Day 37

---
+++ Day 37 – Project 7, part two/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Tolga PIRTURK on 19.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
