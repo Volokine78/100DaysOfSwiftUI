4e3070c2e816b22490d841f71422e65f6b240d1a
Tolga Pirtürk <tpirturk@gmail.com>
2022-03-23 18:26:43 +0300

    Day 39
    Add Moonshot project file.

---
+++ Day 39 – Project 8, part one/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Tolga PIRTURK on 23.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
